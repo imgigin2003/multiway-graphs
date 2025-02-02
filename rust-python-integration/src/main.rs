deac83d2a039f3a9f1199f31ac0a058ea6f84e43
imgigin2003 <callmegigin@gmail.com>
2025-02-02 12:37:05 +0330

    fixed main.rs

--- rust-python-integration/src/main.rs
+++ rust-python-integration/src/main.rs
@@ -14,23 +14,6 @@ fn main() {
         // Initialize the Python interpreter
         Py_Initialize();
 
-        // Define the path to the virtual environment's Python executable
-        let venv_path = "/Users/gigin/Documents/GitHub/multiway-graphs/myenv/lib/python3.13/site-packages";
-        let py_script_path = "/Users/gigin/Documents/GitHub/multiway-graphs/rust-python-integration/py_script";
-        let setup_code = format!(
-    r#"
-import sys
-import os
-sys.path.insert(0, '{}')
-sys.path.insert(0, '{}')
-"#,
-            venv_path, py_script_path
-        );
-
-        // Convert the setup code to a C-style string and execute it
-        let setup_string = CString::new(setup_code).expect("Failed to create CString for setup code.");
-        PyRun_SimpleString(setup_string.as_ptr());
-
         // Path to the external Python file
         let python_file_path = "py_script/main.py";
 
