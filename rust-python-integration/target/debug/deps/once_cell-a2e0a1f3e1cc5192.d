7ef632553f2ddfce1d18611f61e98aa5024fca9b
imgigin2003 <callmegigin@gmail.com>
2025-02-02 13:37:49 +0330

    initialized .gitignore

--- rust-python-integration/target/debug/deps/once_cell-a2e0a1f3e1cc5192.d
+++
@@ -1,9 +0,0 @@
-/Users/gigin/Documents/GitHub/multiway-graphs/rust-python-integration/target/debug/deps/libonce_cell-a2e0a1f3e1cc5192.rmeta: /Users/gigin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.20.2/src/lib.rs /Users/gigin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.20.2/src/imp_std.rs /Users/gigin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.20.2/src/race.rs
-
-/Users/gigin/Documents/GitHub/multiway-graphs/rust-python-integration/target/debug/deps/libonce_cell-a2e0a1f3e1cc5192.rlib: /Users/gigin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.20.2/src/lib.rs /Users/gigin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.20.2/src/imp_std.rs /Users/gigin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.20.2/src/race.rs
-
-/Users/gigin/Documents/GitHub/multiway-graphs/rust-python-integration/target/debug/deps/once_cell-a2e0a1f3e1cc5192.d: /Users/gigin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.20.2/src/lib.rs /Users/gigin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.20.2/src/imp_std.rs /Users/gigin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.20.2/src/race.rs
-
-/Users/gigin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.20.2/src/lib.rs:
-/Users/gigin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.20.2/src/imp_std.rs:
-/Users/gigin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.20.2/src/race.rs:
